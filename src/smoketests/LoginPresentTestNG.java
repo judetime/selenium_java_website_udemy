45c4d52d11186b2ecc2ff58229eb343993fb8c73
judetime <maja.sobierajska@gmail.com>
2018-07-23 09:19:59 +0200

    testng

--- src/smoketests/LoginPresentTestNG.java
+++ src/smoketests/LoginPresentTestNG.java
@@ -1,19 +1,41 @@
 package smoketests;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginPresentTestNG {
 	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		System.out.println("Starting test");
+		driver = utilities.DriverFactory.open("chrome");
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		System.out.println("Closing test");
+		driver.close();
+	}
+	
 	@Test
 	public void loginElementsPresentTest()
 	{
-		WebDriver driver = utilities.DriverFactory.open("chrome");
-		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+		System.out.println("Running test");
+		String webUrl = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
+		driver.get(webUrl);
 		
 		boolean loginEmailBox = driver.findElement(By.id("MainContent_txtUserName")).isDisplayed();
 		boolean passwordBox = driver.findElement(By.id("MainContent_txtPassword")).isDisplayed();
+		
+		Assert.assertTrue(loginEmailBox);
+		Assert.assertTrue(passwordBox);
 		
 		
 		
